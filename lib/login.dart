a31dd644ce38e2c8061363951dca1b14f3da5b63
chosung <apple71228@gmail.com>
2024-09-09 22:03:23 +0900

    feat: sign up page

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:dychat/sign_up_page.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -80,6 +81,16 @@ class _LoginPageState extends State<LoginPage> {
               ElevatedButton(
                 onPressed: _login,
                 child: const Text('로그인'),
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignUpPage()),
+                  );
+                },
+                child: const Text('회원가입'),
               ),
             ],
           ),
